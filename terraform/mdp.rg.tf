eaeca7841f0127fc2abbc01fb5c5d15bfa8c898e
Mahyar Gorji <mahyar.gorji@gov.bc.ca>
2025-02-26 11:33:03 -0500

    added hard dependencies to secrets to wait for KV PE provision

--- terraform/mdp.rg.tf
+++ terraform/mdp.rg.tf
@@ -11,18 +11,3 @@ resource "azurerm_resource_group" "data" {
   name     = "rg-${var.projectNameAbbr}-data-${var.environment}-${var.locationAbbr}"
   location = var.location
 }
-resource "azurerm_management_lock" "core_lock" {
-  name       = "rg-core-lock"
-  lock_level = "CanNotDelete"
-  scope      = azurerm_resource_group.core.id
-}
-resource "azurerm_management_lock" "security_lock" {
-  name       = "rg-sec-lock"
-  lock_level = "CanNotDelete"
-  scope      = azurerm_resource_group.security.id
-}
-resource "azurerm_management_lock" "data_lock" {
-  name       = "rg-data-lock"
-  lock_level = "CanNotDelete"
-  scope      = azurerm_resource_group.data.id
-}

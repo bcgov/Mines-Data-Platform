fe24fa8d39a02e024665f1cac45bd74e0a6cbc72
Mahyar Gorji <mahyar.gorji@gov.bc.ca>
2025-02-28 16:55:47 -0500

    fmt

--- terraform/mdp.secrets.tf
+++ terraform/mdp.secrets.tf
@@ -2,18 +2,18 @@ resource "azurerm_key_vault_secret" "jumpboxadminuser" {
   name         = "jumpboxadminuser"
   value        = "mdpadmin"
   key_vault_id = azurerm_key_vault.kv.id
-  depends_on = [ azurerm_private_endpoint.kv ]
+  depends_on   = [azurerm_private_endpoint.kv]
 }
 
 resource "random_password" "jumpboxadminpassword" {
-  length  = 16
-  special = true
-  depends_on = [ azurerm_private_endpoint.kv ]
+  length     = 16
+  special    = true
+  depends_on = [azurerm_private_endpoint.kv]
 }
 
 resource "azurerm_key_vault_secret" "jumpboxadminpassword" {
   name         = "jumpboxadminpassword"
   value        = random_password.jumpboxadminpassword.result
   key_vault_id = azurerm_key_vault.kv.id
-  depends_on = [ azurerm_private_endpoint.kv ]
+  depends_on   = [azurerm_private_endpoint.kv]
 }

6790c226e6a823db8d06c6e333e4a698e11860b1
Mahyar Gorji <mahyar.gorji@gov.bc.ca>
2025-03-03 17:31:57 -0500

    removed enabled-for-deployment

--- terraform/mdp.output.tf
+++ terraform/mdp.output.tf
@@ -1,8 +1,4 @@
-output "address_prefixes" {
-  value = [for subnet_name in var.subnet_names :
-    cidrsubnet(data.azurerm_virtual_network.vnet.address_space[0], 2, index(var.subnet_names, subnet_name))
-  ]
-}
+
 
 output "vnet_id" {
   value = data.azurerm_virtual_network.vnet.id

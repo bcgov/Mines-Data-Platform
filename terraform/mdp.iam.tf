fc80e2490167132977e093c6491ca8c73d39fc0a
Mahyar Gorji <mahyar.gorji@gov.bc.ca>
2025-03-04 10:23:21 -0500

    removed iam, needs to be separate script

--- terraform/mdp.iam.tf
+++ terraform/mdp.iam.tf
@@ -1,17 +1,17 @@
-resource "azurerm_role_assignment" "datastorageblobcontributor" {
-  scope                = azurerm_storage_account.data.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azuread_client_config.current.object_id
-}
+# resource "azurerm_role_assignment" "datastorageblobcontributor" {
+#   scope                = azurerm_storage_account.data.id
+#   role_definition_name = "Storage Blob Data Contributor"
+#   principal_id         = data.azuread_client_config.current.object_id
+# }
 
-resource "azurerm_role_assignment" "corestorageblobcontributor" {
-  scope                = azurerm_storage_account.core.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = data.azuread_client_config.current.object_id
-}
+# resource "azurerm_role_assignment" "corestorageblobcontributor" {
+#   scope                = azurerm_storage_account.core.id
+#   role_definition_name = "Storage Blob Data Contributor"
+#   principal_id         = data.azuread_client_config.current.object_id
+# }
 
-resource "azurerm_role_assignment" "sp_keyvault_secret_reader" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = data.azuread_client_config.current.object_id
-}
+# resource "azurerm_role_assignment" "sp_keyvault_secret_reader" {
+#   scope                = azurerm_key_vault.kv.id
+#   role_definition_name = "Key Vault Secrets User"
+#   principal_id         = data.azuread_client_config.current.object_id
+# }
--- terraform/mdp.secrets.tf
+++ terraform/mdp.secrets.tf
@@ -2,14 +2,14 @@ resource "azurerm_key_vault_secret" "jumpboxadminuser" {
   name            = "jumpboxadminuser"
   value           = "mdpadmin"
   key_vault_id    = azurerm_key_vault.kv.id
-  depends_on      = [azurerm_private_endpoint.kv, azurerm_role_assignment.sp_keyvault_secret_reader]
+  depends_on      = [azurerm_private_endpoint.kv] //, azurerm_role_assignment.sp_keyvault_secret_reader]
   expiration_date = timeadd(timestamp(), "2160h")
 }
 
 resource "random_password" "jumpboxadminpassword" {
   length     = 16
   special    = true
-  depends_on = [azurerm_private_endpoint.kv, azurerm_role_assignment.sp_keyvault_secret_reader]
+  depends_on = [azurerm_private_endpoint.kv] //, azurerm_role_assignment.sp_keyvault_secret_reader]
 }
 
 resource "azurerm_key_vault_secret" "jumpboxadminpassword" {

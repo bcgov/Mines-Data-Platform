095d238950955f9471ac3d86ec5d028c53ff6dff
Mahyar Gorji <mahyar.gorji@gov.bc.ca>
2025-03-04 22:51:29 -0500

    new kv dependencies

--- terraform/mdp.secrets.tf
+++ terraform/mdp.secrets.tf
@@ -2,7 +2,7 @@ resource "azurerm_key_vault_secret" "jumpboxadminuser" {
   name            = "jumpboxadminuser"
   value           = "mdpadmin"
   key_vault_id    = azurerm_key_vault.kv.id
-  depends_on      = [azurerm_private_endpoint.kv] //, azurerm_role_assignment.sp_keyvault_secret_reader]
+  depends_on      = [azurerm_key_vault.kv, azurerm_private_endpoint.kv] //, azurerm_role_assignment.sp_keyvault_secret_reader]
   expiration_date = timeadd(timestamp(), "2160h")
 }
 
@@ -16,6 +16,6 @@ resource "azurerm_key_vault_secret" "jumpboxadminpassword" {
   name            = "jumpboxadminpassword"
   value           = random_password.jumpboxadminpassword.result
   key_vault_id    = azurerm_key_vault.kv.id
-  depends_on      = [azurerm_private_endpoint.kv]
+  depends_on      = [azurerm_key_vault.kv, azurerm_private_endpoint.kv]
   expiration_date = timeadd(timestamp(), "2160h")
 }

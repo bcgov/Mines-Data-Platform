96dc548f798e9770225ac5340de0498acb876770
Mahyar Gorji <mahyar.gorji@gov.bc.ca>
2025-03-04 22:00:45 -0500

    stg accounts attempt 2

--- terraform/mdp.storage.tf
+++ terraform/mdp.storage.tf
@@ -1,46 +1,57 @@
 resource "azurerm_storage_account" "core" {
-  name                          = lower("stg${var.projectNameAbbr}core${var.environment}")
-  resource_group_name           = azurerm_resource_group.core.name
-  location                      = var.location
-  account_tier                  = "Standard"
-  account_replication_type      = "LRS"
-  public_network_access_enabled = false
-  https_traffic_only_enabled    = true
-  sftp_enabled                  = false
-  min_tls_version               = "TLS1_2"
+  name                     = lower("stg${var.projectNameAbbr}core${var.environment}")
+  resource_group_name      = azurerm_resource_group.core.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  access_tier              = "Hot"
+
+  https_traffic_only_enabled = true
+  min_tls_version            = "TLS1_2"
 
   network_rules {
-    default_action             = "Deny"
-    bypass                     = ["None"]
-    virtual_network_subnet_ids = [data.azurerm_subnet.privatesubnet.id]
+    default_action = "Deny"
+    bypass         = ["None"]
+    virtual_network_subnet_ids = [
+      data.azurerm_subnet.privatesubnet.id,
+    ]
   }
+
+  tags = var.tags
+
   lifecycle {
     ignore_changes = [
       tags
     ]
   }
-  tags = var.tags
 }
 
 resource "azurerm_storage_account" "data" {
-  name                          = lower("stg${var.projectNameAbbr}data${var.environment}")
-  resource_group_name           = azurerm_resource_group.data.name
-  location                      = var.location
-  account_tier                  = "Standard"
-  account_replication_type      = "LRS"
-  public_network_access_enabled = false
-  sftp_enabled                  = false
-  https_traffic_only_enabled    = true
-  min_tls_version               = "TLS1_2"
+  name                     = lower("stg${var.projectNameAbbr}data${var.environment}")
+  resource_group_name      = azurerm_resource_group.data.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  access_tier              = "Hot"
+
+  https_traffic_only_enabled = true
+  min_tls_version            = "TLS1_2"
+
   network_rules {
-    default_action             = "Deny"
-    bypass                     = ["None"]
-    virtual_network_subnet_ids = [data.azurerm_subnet.privatesubnet.id]
+    default_action = "Deny"
+    bypass         = ["None"]
+    virtual_network_subnet_ids = [
+      data.azurerm_subnet.privatesubnet.id,
+    ]
   }
+
+  tags = var.tags
+
   lifecycle {
     ignore_changes = [
       tags
     ]
   }
-  tags = var.tags
 }

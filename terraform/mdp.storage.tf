603c3d9949c6756163841b6a3046459aecf67859
Mahyar Gorji <mahyar.gorji@gov.bc.ca>
2025-03-04 22:08:14 -0500

    stg acc changes attempt 3

--- terraform/mdp.storage.tf
+++ terraform/mdp.storage.tf
@@ -1,11 +1,14 @@
 resource "azurerm_storage_account" "core" {
-  name                     = lower("stg${var.projectNameAbbr}core${var.environment}")
-  resource_group_name      = azurerm_resource_group.core.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-  access_tier              = "Hot"
+  name                            = lower("stg${var.projectNameAbbr}core${var.environment}")
+  resource_group_name             = azurerm_resource_group.core.name
+  location                        = var.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  account_kind                    = "StorageV2"
+  access_tier                     = "Hot"
+  public_network_access_enabled   = false
+  allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = false
 
   https_traffic_only_enabled = true
   min_tls_version            = "TLS1_2"
@@ -28,16 +31,18 @@ resource "azurerm_storage_account" "core" {
 }
 
 resource "azurerm_storage_account" "data" {
-  name                     = lower("stg${var.projectNameAbbr}data${var.environment}")
-  resource_group_name      = azurerm_resource_group.data.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-  access_tier              = "Hot"
-
-  https_traffic_only_enabled = true
-  min_tls_version            = "TLS1_2"
+  name                            = lower("stg${var.projectNameAbbr}data${var.environment}")
+  resource_group_name             = azurerm_resource_group.data.name
+  location                        = var.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  account_kind                    = "StorageV2"
+  access_tier                     = "Hot"
+  public_network_access_enabled   = false
+  https_traffic_only_enabled      = true
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = false
 
   network_rules {
     default_action = "Deny"

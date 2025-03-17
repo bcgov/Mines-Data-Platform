9f2a31ecf467daf2086c72828b261b6139a45021
Mahyar Gorji <mahyar.gorji@gov.bc.ca>
2025-03-17 15:48:32 -0400

    added query debug echo

--- scripts/createbackend.sh
+++ scripts/createbackend.sh
@@ -11,9 +11,10 @@ RESOURCE_GROUP_NAME="rg-mdp-unmanaged-$ENVIRONMENT-ca"
 STORAGE_ACCOUNT_NAME="stgmdptfstate${ENVIRONMENT}ca"
 CONTAINER_NAME="tfstate"
 SUBSCRIPTION_NAME=$LICENSE_PLATE-$ENVIRONMENT
+echo "SUB QUERY=$(az account list --query "[?name=='$SUBSCRIPTION_NAME'].id" -o tsv)"
 SUBSCRIPTION_ID=$(az account list --query "[?name=='$SUBSCRIPTION_NAME'].id" -o tsv)
 
-echo "Subscription ID: $$SUBSCRIPTION_ID}"
+echo "SUBSCRIPTION ID: $$SUBSCRIPTION_ID}"
 
 ACCOUNT_TYPE=$(az account show --query user.type -o tsv)
 echo $ACCOUNT_TYPE

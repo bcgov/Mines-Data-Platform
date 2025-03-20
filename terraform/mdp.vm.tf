45ae23c40bae6d0602c6bcefe7c4259e58166e3e
Mahyar Gorji <mahyar.gorji@gov.bc.ca>
2025-03-20 14:41:22 -0400

    username vm

--- terraform/mdp.vm.tf
+++ terraform/mdp.vm.tf
@@ -4,8 +4,9 @@ resource "azurerm_windows_virtual_machine" "shir" {
   computer_name       = "vm-${var.projectNameAbbr}-jump"
   location            = azurerm_resource_group.core.location
   size                = var.jumpbox_SKU
-  admin_username      = azurerm_key_vault_secret.jumpboxadminuser.value
-  admin_password      = azurerm_key_vault_secret.jumpboxadminpassword.value
+  # admin_username      = azurerm_key_vault_secret.jumpboxadminuser.value
+  admin_username = "mdpadmin"
+  admin_password = azurerm_key_vault_secret.jumpboxadminpassword.value
   network_interface_ids = [
     azurerm_network_interface.shir.id,
   ]

347e702d69ee954c110a026c25ab3aa1deb137a0
Mahyar Gorji <mahyar.gorji@gov.bc.ca>
2025-09-17 10:21:06 -0400

    added data tag

--- terraform/mdp.rbac.tf
+++ terraform/mdp.rbac.tf
@@ -1,11 +1,11 @@
 resource "azurerm_role_assignment" "kv_sp_contributor" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Contributor"
-  principal_id         = azuread_client_config.current.object_id
+  principal_id         = data.azuread_client_config.current.object_id
 }
 
 resource "azurerm_role_assignment" "kv_secrets_officer" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = azuread_client_config.current.object_id
+  principal_id         = data.azuread_client_config.current.object_id
 }

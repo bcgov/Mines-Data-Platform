c1627a288e13b9e2508ef294728b6798eeff500f
Mahyar Gorji <mahyar.gorji@gov.bc.ca>
2025-10-15 15:33:03 -0400

    [no ci] private subnet data source -> resource

--- terraform/mdp.subnets.tf
+++ terraform/mdp.subnets.tf
@@ -1,34 +1,58 @@
-# resource "azapi_resource" "publicsubnet" {
-#   type      = "Microsoft.Network/virtualNetworks/subnets@2024-03-01"
-#   name      = "sn-${var.projectNameAbbr}-public-${var.environment}-${var.locationAbbr}"
-#   parent_id = data.azurerm_virtual_network.vnet.id
-#   locks     = [data.azurerm_virtual_network.vnet.id]
-#   body = {
-#     properties = {
-#       addressPrefix = cidrsubnet(data.azurerm_virtual_network.vnet.address_space[0], 2, 3)
-#       networkSecurityGroup = {
-#         id = azurerm_network_security_group.public_nsg.id
-#       }
-#       serviceEndpoints = [
-#         {
-#           service = "Microsoft.KeyVault"
-#         },
-#         {
-#           service = "Microsoft.Storage"
-#         }
-#       ]
-#     }
-#   }
+resource "azapi_resource" "publicsubnet" {
+  type      = "Microsoft.Network/virtualNetworks/subnets@2024-03-01"
+  name      = "sn-${var.projectNameAbbr}-public-${var.environment}-${var.locationAbbr}"
+  parent_id = data.azurerm_virtual_network.vnet.id
+  locks     = [data.azurerm_virtual_network.vnet.id]
+  body = {
+    properties = {
+      addressPrefix = cidrsubnet(data.azurerm_virtual_network.vnet.address_space[0], 2, 1)
+      networkSecurityGroup = {
+        id = azurerm_network_security_group.public_nsg.id
+      }
+      serviceEndpoints = [
+        {
+          service = "Microsoft.KeyVault"
+        },
+        {
+          service = "Microsoft.Storage"
+        }
+      ]
+    }
+  }
 
-#   depends_on = [azurerm_network_security_group.public_nsg]
-# }
-
-#TODO: Created as a part of a script, will be reintroduced here later as a terraform resource
-data "azurerm_subnet" "privatesubnet" {
-  name                 = "sn-${var.projectNameAbbr}-private-${var.environment}-${var.locationAbbr}"
-  virtual_network_name = data.azurerm_virtual_network.vnet.name
-  resource_group_name  = "${var.licensePlate}-${var.environment}-networking"
+  depends_on = [azurerm_network_security_group.public_nsg]
 }
+
+resource "azapi_resource" "privatesubnet" {
+  type      = "Microsoft.Network/virtualNetworks/subnets@2024-03-01"
+  name      = "sn-${var.projectNameAbbr}-private-${var.environment}-${var.locationAbbr}"
+  parent_id = data.azurerm_virtual_network.vnet.id
+  locks     = [data.azurerm_virtual_network.vnet.id]
+  body = {
+    properties = {
+      addressPrefix = cidrsubnet(data.azurerm_virtual_network.vnet.address_space[0], 2, 2)
+      networkSecurityGroup = {
+        id = azurerm_network_security_group.private_nsg.id
+      }
+      serviceEndpoints = [
+        {
+          service = "Microsoft.KeyVault"
+        },
+        {
+          service = "Microsoft.Storage"
+        }
+      ]
+    }
+  }
+
+  depends_on = [azurerm_network_security_group.public_nsg]
+}
+
+# data "azurerm_subnet" "privatesubnet" {
+#   name                 = "sn-${var.projectNameAbbr}-private-${var.environment}-${var.locationAbbr}"
+#   virtual_network_name = data.azurerm_virtual_network.vnet.name
+#   resource_group_name  = "${var.licensePlate}-${var.environment}-networking"
+# }
 
 resource "azapi_resource" "bastion_subnet" {
   type = "Microsoft.Network/virtualNetworks/subnets@2024-05-01"
@@ -41,7 +65,7 @@ resource "azapi_resource" "bastion_subnet" {
 
   body = {
     properties = {
-      addressPrefix = var.bastionSubnetAddressPrefix
+      addressPrefix = var.bastionSubnetAddressPrefix #bastion takes the priority with /26 subnet size
       networkSecurityGroup = {
         id = azurerm_network_security_group.bastion_nsg.id
       }

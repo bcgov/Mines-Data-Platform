9bbdfa464f3bd7d2a820885d2f15527538e23fe3
Mahyar Gorji <mahyar.gorji@gov.bc.ca>
2025-10-15 09:10:50 -0400

    removed scripts folder, added auth-type to az login

--- scripts/statefirewalldelete.sh
+++
@@ -1,2 +0,0 @@
-CURRENT_IP=$(curl -s https://api.ipify.org)
-  az storage account network-rule remove --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --ip-address $CURRENT_IP
